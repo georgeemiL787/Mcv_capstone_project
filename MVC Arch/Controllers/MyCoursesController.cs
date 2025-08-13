1fda7fd6d5a94ec7d9457779f4f2490b9b291f71
georgeemiL787 <128412365+georgeemiL787@users.noreply.github.com>
2025-08-13 10:21:33 +0300

    .

--- MVC Arch/Controllers/MyCoursesController.cs
+++ MVC Arch/Controllers/MyCoursesController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using MCV_Capstone.Attributes;
 
 namespace MCV_Capstone.Controllers
 {
+    [RequireRole("Student")]
     public class MyCoursesController : Controller
     {
         public IActionResult Index()
